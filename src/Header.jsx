2a7ac2aa9963c179375ee66375dc4270111cc86d
xsaynt <alex-price3@hotmail.co.uk>
2024-12-18 17:04:45 +0000

    Working on CSS before moving on to sorting articles

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,11 +1,16 @@
 import { Link } from 'react-router-dom';
+import ncLogo from '../ncLogo.png';
 
 export const Header = () => {
 	return (
 		<header>
-			<h1>
-				<Link to={`/`}>NC News</Link>
-			</h1>
+			<section className='user-profile'>
+				<h1>
+					<Link to={`/`}>NC News</Link>
+				</h1>
+				<img src={ncLogo} alt='Northcoders Logo' className='website-logo' />
+			</section>
+			<p className='username'>cooljmessy</p>
 		</header>
 	);
 };
